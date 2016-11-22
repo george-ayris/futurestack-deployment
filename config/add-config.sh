982772203f43f3e87357a8db64468c70e8b7be59
George Ayris <george.ayris@huddle.com>
2016-11-22 18:50:11 +0000

    Db connection string to config

--- config/add-config.sh
+++ config/add-config.sh
@@ -1,1 +1,2 @@
 curl -X PUT -d 'test' http://localhost:8500/v1/kv/web/key1
+curl -X PUT -d 'Server=tcp:futurestack.database.windows.net,1433;Initial Catalog=FutureStack;Persist Security Info=False;User ID={0};Password={1};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;' http://localhost:8500/v1/kv/web/sqlConnectionString
